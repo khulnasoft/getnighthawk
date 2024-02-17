de110b7d9c5e5244376cd061710c20eb36bae54e
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-02-17 06:17:04 +0000

    init commit

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-NIGHTHAWK_DIR="/home/runner/work/getnighthawk/getnighthawk/"
-cd $NIGHTHAWK_DIR
-
-if ! bazel build -c opt --define tcmalloc=gperftools //:nighthawk; then
-  printf "ERROR\tUnable to build nighthawk client\n"
-  exit 1;
-fi
